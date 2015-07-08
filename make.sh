4f1ada5fd0b13b5cc31f65149d0b07ac28703f35
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-07-08 14:38:01 +0200

    Depend on phantomjs

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+basedir=`dirname $0`
+
 for file in `ls raw`
 do
   echo -n "Converting raw/$file to geojson "
-  ./poi_dump_to_geojson.sh raw/$file input/${file}.geojson
+  $basedir/poi_dump_to_geojson.sh raw/$file input/${file}.geojson
   echo
 done
 
-phantomjs generate-clusters.js
+$basedir/node_modules/phantomjs/bin/phantomjs generate-clusters.js
