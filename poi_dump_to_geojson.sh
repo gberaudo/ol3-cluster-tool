0f4c1b98b8f3d0deacfce1594492d4d638014c8c
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-07-08 11:36:55 +0200

    Speed up poi dump to geojson input conversion

--- poi_dump_to_geojson.sh
+++ poi_dump_to_geojson.sh
@@ -5,12 +5,9 @@ filein=$1
 fileout=${2-"$1.geojson"}
 
 echo '{"type":"FeatureCollection", "features": [' > $fileout
-while read p
+while IFS="|" read geom id
 do
-  geom=`echo $p | cut -d\| -f 1`
-  id=`echo $p | cut -d\| -f 2`
   echo "{\"geometry\": $geom, \"type\":\"Feature\", \"id\":$id}," >> $fileout
-  echo -n "."
 done < $filein
 sed -i '$s/.$//' $fileout
 echo ']}' >> $fileout
